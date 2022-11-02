7f2b6cccd745249861c02a5f0fd89c52b2fe6e21
chandur-surge <chandur@surge.global>
2022-11-02 17:18:58 +0530

    implemented header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,44 +1,44 @@
-import React from 'react'
-import { Route } from 'react-router-dom' 
+import React from "react";
+import { Route } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux'
-import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
-import SearchBox from './SearchBox' 
-import { logout } from '../actions/userActions'
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import SearchBox from "./SearchBox";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
   const history = useHistory();
 
   const logoutHandler = () => {
-    dispatch(logout())
+    dispatch(logout());
     history.push("/");
-  }
+  };
 
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
+          <LinkContainer to="/">
             <Navbar.Brand>Bulkart</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-          <Route render={({ history }) => <SearchBox history={history} />} /> 
-            <Nav className='ml-auto'>
-              <LinkContainer to='/cart'>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Route render={({ history }) => <SearchBox history={history} />} />
+            <Nav className="ml-auto">
+              <LinkContainer to="/cart">
                 <Nav.Link>
-                  <i className='fas fa-shopping-cart'></i> Cart
+                  <i className="fas fa-shopping-cart"></i> Cart
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
-                <NavDropdown title={userInfo.name} id='username'>
-                  <LinkContainer to='/profile'>
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
                   <NavDropdown.Item onClick={logoutHandler}>
@@ -46,21 +46,21 @@ const Header = () => {
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <LinkContainer to='/login'>
+                <LinkContainer to="/login">
                   <Nav.Link>
-                    <i className='fas fa-user'></i> Sign In
+                    <i className="fas fa-user"></i> Sign In
                   </Nav.Link>
                 </LinkContainer>
               )}
               {userInfo && userInfo.isVendor && (
-                <NavDropdown title='Products' id='adminmenu'>
-                  <LinkContainer to='/admin/productWaitinglist'>
+                <NavDropdown title="Products" id="adminmenu">
+                  <LinkContainer to="/admin/productWaitinglist">
                     <NavDropdown.Item>Waiting</NavDropdown.Item>
-                  </LinkContainer> 
-                  <LinkContainer to='/admin/productDispatchReady'>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/productDispatchReady">
                     <NavDropdown.Item>Ready to Dispatch</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to='/admin/productDispatched'>
+                  <LinkContainer to="/admin/productDispatched">
                     <NavDropdown.Item>Dispatched Products</NavDropdown.Item>
                   </LinkContainer>
                 </NavDropdown>
@@ -70,7 +70,7 @@ const Header = () => {
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
